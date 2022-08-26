b28b0106055c208fa535d47c56965d4c34d892c6
james-arn <j1mes@hotmail.co.uk>
2022-08-26 11:22:03 +0100

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,24 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   template: `
-  <h3> Cart </h3>
-  <div 
-  class="cart-item"
-  *ngFor="let item of items">
-    <span>{{ item.name }}</span>
-    <span>{{ item.price | currency }}</span>
+  <h3>Cart</h3>
+
+<p>
+  <a routerLink="/shipping">Shipping Prices</a>
+</p>
+
+<div class="cart-item" *ngFor="let item of items">
+  <span>{{ item.name }} </span>
+  <span>{{ item.price | currency }}</span>
+</div>
+
+<form [formGroup]="checkoutForm" (ngSubmit)="onSubmit()">
+
+  <div>
+    <label for="name">
+      Name
+    </label>
+    <input id="name" type="text" formControlName="name">
   </div>
-  <a routerLink="/shipping">
-  Shipping prices
-  </a>
+
+  <div>
+    <label for="address">
+      Address
+    </label>
+    <input id="address" type="text" formControlName="address">
+  </div>
+
+  <button class="button" type="submit">Purchase</button>
+
+</form>
   `,
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
   items = this.cartService.getCartItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCartItems();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    window.alert('checked console for order');
+    this.checkoutForm.reset();
+  }
 }
